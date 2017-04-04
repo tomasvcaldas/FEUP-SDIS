495e7f7e47e3f695be77331e8b95046d1a40a446
joaochaves96 <joaovitor1906@hotmail.com>
2017-04-04 13:45:33 +0100

    Hash function added

---
+++ project_1/src/Utilities/Hash.java
@@ -0,0 +1,23 @@
+package Utilities;
+
+import java.security.MessageDigest;
+
+public class Hash {
+    public static String sha256(String fileID){
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            md.update(fileID.getBytes("UTF-8"));
+            byte[] digest = md.digest();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < digest.length; i++) {
+                sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+
+        }catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+}
